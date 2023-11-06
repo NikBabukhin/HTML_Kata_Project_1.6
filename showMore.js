d739a1f8f0cbf968d92d2eba44773c87f3838437
NikBabukhin <6opsh50@gmail.com>
2023-11-06 13:25:50 +0300

    Change show more to list and fix show more entity

--- showMore.js
+++ showMore.js
@@ -1,64 +1,8 @@
-const wrapper = document.getElementById('brands-wrapper-except-mobile')
-const wrapperChild = wrapper.children
-const buttonShow = document.getElementById('add-brands')
-const imagesArr = ['lenovo', 'samsung', 'apple', 'acer', 'hp', 'Borsch']
-const initialSlidesLength = 6;
-const allProductsLength = 11;
+const brandsWrapper = document.getElementById('brands-wrapper-except-mobile')
+const buttonShowMore = document.getElementById('add-brands')
 
-const randomIndex = (maxLength) => Math.floor(Math.random() * maxLength)
-
-const createNewCard = (element, imageSrc) => {
-    const productCard = document.createElement('div')
-    productCard.classList.add('company-card')
-    productCard.classList.add('company-card--laptop')
-
-    const cardLogo = document.createElement('div')
-    cardLogo.classList.add('company-card__logo')
-    cardLogo.innerHTML = `<img src="src/brands/${imageSrc}.png" alt="brand image">`
-
-    const cardLink = document.createElement('a')
-    cardLink.classList.add('header-button')
-    cardLink.classList.add('go-background')
-    cardLink.innerHTML = `<a href="https://github.com/NikBabukhin/" target="_blank"></a>`
-
-    productCard.appendChild(cardLogo)
-    productCard.appendChild(cardLink)
-
-    element.appendChild(productCard)
-}
-const deleteNewCard = (element) => {
-    const lastElement = element[element.length-1]
-    lastElement.remove()
+const showMoreClickHandler = (showElement) => {
+    showElement.classList.toggle('show')
 }
 
-const showMore = (initialCount, finalCount, element) => {
-    for (let i=initialCount; i<finalCount; i++) {
-        createNewCard(element, imagesArr[randomIndex(imagesArr.length)])
-    }
-}
-const showLess = (initialCount, finalCount) => {
-    for (let i=0; i<finalCount-initialCount; i++) {
-        deleteNewCard(wrapperChild)
-    }
-}
-
-const onClickHandler = (initialSlidesLength, allProductsLength) => {
-    const currentLength = wrapper.children.length;
-    const buttonSpan = buttonShow.querySelector('.show-more__button-text')
-    if (currentLength>=allProductsLength) {
-        buttonSpan.textContent = 'Показать все';
-        buttonShow.classList.remove('down')
-        return showLess(initialSlidesLength, allProductsLength)
-    } else {
-        buttonShow.classList.add('down')
-        buttonSpan.textContent = 'Скрыть';
-        return showMore(initialSlidesLength, allProductsLength, wrapper)
-    }
-}
-
-for (let i=0;i<initialSlidesLength;i++) {
-    createNewCard(wrapper, imagesArr[randomIndex(imagesArr.length)])
-}
-
-
-buttonShow.addEventListener('click', () => onClickHandler(initialSlidesLength, allProductsLength))
+buttonShowMore.addEventListener('click', ()=>showMoreClickHandler(brandsWrapper))
