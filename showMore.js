62f21b7ffdd0ae14535e6b3ec4cf9583132cabc3
NikBabukhin <6opsh50@gmail.com>
2023-11-09 18:29:07 +0300

    Fix svg icons for button. Fix some problems with grid-template

--- showMore.js
+++ showMore.js
@@ -2,6 +2,10 @@ const brandsWrapper = document.getElementById('brands-wrapper-except-mobile')
 const buttonShowMore = document.getElementById('add-brands')
 
 const showMoreClickHandler = (showElement) => {
+    const buttonSpan = buttonShowMore.querySelector('.show-more__button-text')
+    const backgroundImage = buttonShowMore.querySelector(':before')
+    console.log(backgroundImage)
+    buttonSpan.textContent = buttonSpan.textContent === 'Скрыть' ? 'Показать все' : 'Скрыть';
     showElement.classList.toggle('show')
 }
 
