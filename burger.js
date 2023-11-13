68292fad4676f979e94f17ece74f15a47e0c630a
NikBabukhin <6opsh50@gmail.com>
2023-11-13 14:50:56 +0300

    Add some fixes for JS and HTML

--- burger.js
+++ burger.js
@@ -1,17 +1,26 @@
 const closeBurger = () => {
     burgerMenu.classList.remove('burger-open')
+    blurArea.classList.remove('aside--open')
 }
-
 const openBurger = () => {
     burgerMenu.classList.add('burger-open')
+    blurArea.classList.add('aside--open')
 }
 
+
+const blurArea = document.querySelector('.aside')
 const burgerMenu = document.getElementById("burger_menu")
 const burgerCloseButton = document.getElementById("burger_close")
 const burgerOpenButton = document.getElementById("burger_open")
 
+
+burgerMenu.addEventListener('click', (e) => {
+    e.stopPropagation()
+})
 burgerCloseButton.addEventListener('click', closeBurger)
 burgerOpenButton.addEventListener('click', openBurger)
+blurArea.addEventListener('click', () => closeBurger())
+
 
 
 
--- showMore.js
+++ showMore.js
@@ -3,8 +3,6 @@ const buttonShowMore = document.getElementById('add-brands')
 
 const showMoreClickHandler = (showElement) => {
     const buttonSpan = buttonShowMore.querySelector('.show-more__button-text')
-    const backgroundImage = buttonShowMore.querySelector(':before')
-    console.log(backgroundImage)
     buttonSpan.textContent = buttonSpan.textContent === 'Скрыть' ? 'Показать все' : 'Скрыть';
     showElement.classList.toggle('show')
 }
