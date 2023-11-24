0198da5a89eb42c88f34b44915a1b11b19c6411c
NikBabukhin <6opsh50@gmail.com>
2023-11-24 15:14:50 +0300

    Cnahge buton span class form margin to gap

--- src/scripts/showMore.js
+++ src/scripts/showMore.js
@@ -1,6 +1,6 @@
 export const showMoreClickHandler = (showElement, buttonElement, showClassName, buttonText) => {
-    const buttonSpan = buttonElement.querySelector('.show-more__button-text')
-    buttonSpan.textContent = buttonSpan.textContent === 'Скрыть' ? buttonText : 'Скрыть';
+    const buttonSpan = buttonElement.querySelector('span')
+    buttonSpan.innerText = buttonSpan.innerText === 'Скрыть' ? buttonText : 'Скрыть';
     buttonElement.querySelector('.show-more__button-image').classList.toggle('show-more__button-image-reverse');
     showElement.classList.toggle(showClassName)
 }
